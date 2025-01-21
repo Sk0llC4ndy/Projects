8968e7d787578c35450e3cabfcf3f84068a1ea6a
Sk0llC4ndy <Tylerj.192005@gmail.com>
2025-01-20 20:05:30 -0600

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+// Tyler Ward
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!"); //greetings
+        System.out.println("Job title: Information Security Analyst"); //job title
+        System.out.println("Why: I enjoy security-oriented work"); //reasoning
+        System.out.println("Median salary: $120,360"); //salary
+    }
+}
