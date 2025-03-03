fcadc58b624074a156d484291b4797e8240a0758
Sk0llC4ndy <Tylerj.192005@gmail.com>
2025-03-02 21:02:47 -0600

    Update Calc.java
    added array

--- Calc.java
+++ Calc.java
@@ -1,58 +1,63 @@
+import java.text.DecimalFormat;
+//Import for correcting the quotient decimal formatting
+//Welcome to the Calculator program
+//insert two numbers and the calculator with catanate the results of sum, subtraction, multiplying, and division
 public class Calc {
-    private double Num1;
-    private double Num2;
+    private double [][] numbers;
 
-    // Custom constructor (no-arg)
+    // Custom constructor (size = argument)
     public Calc() {
-        this.Num1 = 0;
-        this.Num2 = 0;
+        this.numbers = new double[1][2];
     }
 
     // Getters
-    public double getNum1() {
-        return Num1;
+    public double getNum1(int index) {
+        return numbers[index][0];
     }
 
-    public double getNum2() {
-        return Num2;
+    public double getNum2(int index) {
+        return numbers[index][1];
     }
 
     // Setters
-    public void setNum1(double Num1) {
-        this.Num1 = Num1;
+    public void setNum1(int index, double num1) {
+        this.numbers[index][0] = num1;
     }
 
-    public void setNum2(double Num2) {
-        this.Num2 = Num2;
+    public void setNum2(int index, double num2) {
+        this.numbers[index][1] = num2;
     }
-
-    // Calculate sum
-    public double add() {
-        return Num1 + Num2;
+    
+        // Calculate sum
+    public double add(int index) {
+        return numbers[index][0] + numbers[index][1];
     }
-
+    
     // Calculate subtraction
-    public double subtract() {
-        return Num1 - Num2;
+    public double subtract(int index) {
+        return numbers[index][0] - numbers[index][1];
     }
-
     // Calculate multiplication
-    public double multiply() {
-        return Num1 * Num2;
+    public double multiply(int index) {
+        return numbers[index][0] * numbers[index][1];
     }
-
+    
     // Calculate division (with zero check)
-    public double divide() {
-        if (Num2 == 0) {
-            System.out.println("Error: Cannot divide by zero.");
+      public double divide(int index) {
+        if (numbers[index][1] == 0) {
+            System.out.println("Error: Cannot divide by zero at index " + index);
             return Double.NaN; // NaN = "Not a Number"
         }
-        return Num1 / Num2;
+        return numbers[index][0] / numbers[index][1];
     }
-
+    
     // Override toString to display numbers
     @Override
     public String toString() {
-        return "First number: " + Num1 + ", Second number: " + Num2;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < numbers.length; i++) {
+            result.append("Index " + i + ": Num1 = " + numbers[i][0] + ", Num2 = " + numbers[i][1] + "\n");
+        }
+        return result.toString();
     }
 }
