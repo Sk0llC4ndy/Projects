90f73340da43b9b6ce271336ce8297c7475d3b44
Sk0llC4ndy <Tylerj.192005@gmail.com>
2025-03-30 21:50:58 -0500

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,28 @@
+public class Exam extends Assessment { 
+   private int NumQuestions; // total number of questions
+   private int NumMissed; // Number of Missed Questions
+   private double pointsEach; //Points that the questions are worth
+   
+   /** 
+   Constructor to intialize exam
+   * Params total number of questions
+   * Params number of not missed questions
+   */
+   public Exam(int questions, int missed) {
+   this.NumQuestions = questions;
+   this.NumMissed = missed;
+   this.pointsEach = 100.0 / questions; //Points of each question depends on size
+   
+   //calculating score
+   int score = (int) Math.round(100.0 - (missed * pointsEach));
+   setScore(score); //Stores score within the superclass
+ }
+ 
+ /** 
+ * Returns the points each question is worth
+ * return points per question.
+ */
+ public double getPointsEach() {
+   return pointsEach;
+ }
+}
