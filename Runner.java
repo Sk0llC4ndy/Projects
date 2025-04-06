c94b77224e3e180beecf6941f7476002505bec6e
Sk0llC4ndy <Tylerj.192005@gmail.com>
2025-04-05 23:07:13 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,38 +1,52 @@
-//this class tests all methods in the Calc class
-//the Calc class exposes the following methods:
-//add, subtract, multiply, divide
-//user is prompted for input
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ This program demonstrates the Exam class,
+ which extends the Assessment class.
+ */
+
 public class Runner {
-    public static void main(String[] args){
-        //instantiate a Calc object
-        Calc myCalculator = new Calc();
-        //get user input for two numbers
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the first number: ");
-        double n1 = scan.nextDouble();
-        System.out.println("Please enter the second number: ");
-        double n2 = scan.nextDouble();
 
-        //pass the numbers to the Calc object
-        myCalculator.setNum1(n1);
-        myCalculator.setNum2(n2);
+        public static void main(String[] args)
+        {
+            String input;     // To hold input
+            int questions;    // Number of questions
+            int missed;       // Number of questions missed
+            boolean validInput = false;
+            
+            while (!validInput) {
+               try {
+                   // Get the number of questions on the exam
+                   qinput = JOptionPane.showInputDialog("How many questions are on the exam?");
+                   questions = Integer.parseInt(qinput);
 
-        //output from Calc instance
-        System.out.println(myCalculator);
+                   // Get the number of questions the student missed
+                   minput = JOptionPane.showInputDialog("How many questions did the student miss?");
+                   missed = Integer.parseInt(minput);
+                   
+                   if (questions <= 0) {
+                      throw new ArithmeticException("Total number of questions must be greater than zero.");
+                   }
+                   
+                   validInput = true; //Input is valid, break the Loop
+                   } catch (NumberFormatException e) {
+                     JOptionPane.showMessageDialog(null, "Invalid input. Please enter integers only.");
+                   } catch (ArithmeticException e) {
+                     JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+                   }
+            }
+              
 
-        //examining the instance private data fields by calling get methods
-        System.out.println("Calling num1 get method: " + myCalculator.getNum1());
-        System.out.println("Calling num2 get method: " + myCalculator.getNum2());
+            // Create an Exam object
+            Exam exam = new Exam(questions, missed);
 
-        //calling Calc methods directly
-        //can use a local variable for sum if you need that value for another calculation
-        double sum = myCalculator.add();
-        System.out.println("The sum is: " + sum);
+            // Display the test results
+            String message = "Each question counts " + exam.getPointsEach();
+            message += " points.\nThe exam score is " + exam.getScore();
+            message += "\nThe exam grade is " + exam.getGrade();
+            JOptionPane.showMessageDialog(null, message);
 
-        //or if you don't, then just display the difference, product and quotient
-        System.out.println("The difference is: " + myCalculator.subtract());
-        System.out.println("The product is: " + myCalculator.multiply());
-        System.out.println("The quotient is: " + myCalculator.divide());
+            System.exit(0);
+        }
     }
-}
+
+
