400e51c0aeecfd026632a29047a647abb567a9c6
Sk0llC4ndy <Tylerj.192005@gmail.com>
2025-04-20 22:49:38 -0500

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -1,52 +1,5 @@
-import javax.swing.JOptionPane;
-/**
- This program demonstrates the Exam class,
- which extends the Assessment class.
- */
-
 public class Runner {
-
-        public static void main(String[] args)
-        {
-            String input;     // To hold input
-            int questions;    // Number of questions
-            int missed;       // Number of questions missed
-            boolean validInput = false;
-            
-            while (!validInput) {
-               try {
-                   // Get the number of questions on the exam
-                   qinput = JOptionPane.showInputDialog("How many questions are on the exam?");
-                   questions = Integer.parseInt(qinput);
-
-                   // Get the number of questions the student missed
-                   minput = JOptionPane.showInputDialog("How many questions did the student miss?");
-                   missed = Integer.parseInt(minput);
-                   
-                   if (questions <= 0) {
-                      throw new ArithmeticException("Total number of questions must be greater than zero.");
-                   }
-                   
-                   validInput = true; //Input is valid, break the Loop
-                   } catch (NumberFormatException e) {
-                     JOptionPane.showMessageDialog(null, "Invalid input. Please enter integers only.");
-                   } catch (ArithmeticException e) {
-                     JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
-                   }
-            }
-              
-
-            // Create an Exam object
-            Exam exam = new Exam(questions, missed);
-
-            // Display the test results
-            String message = "Each question counts " + exam.getPointsEach();
-            message += " points.\nThe exam score is " + exam.getScore();
-            message += "\nThe exam grade is " + exam.getGrade();
-            JOptionPane.showMessageDialog(null, message);
-
-            System.exit(0);
-        }
+    public static void main(String[] args) {
+        Towers towersOfHanoi = new Towers(3);
     }
-
-
+}
---
+++ Towers.java
@@ -0,0 +1,26 @@
+public class Towers {
+    //Solution to the Tower of Hanoi game.
+    private int numDiscs;
+
+    //Constructor param - the number of discs to use
+    //constructor calls a recursive method
+    public Towers(int n) {
+        this.numDiscs = n;
+
+        //Move the number of discs from peg 1 to peg 3
+        //use peg 2 as temporary storage location
+        //calling recursive method
+        this.moveDiscs(this.numDiscs, 1, 3, 2);
+    }
+
+    //The moveDiscs method displays a disc move using a recursive algorithm
+    private void moveDiscs(int num, int fromPeg, int toPeg, int tempPeg) {
+        if (num > 0) {
+            //recursive call -creates another whole copy of method
+            moveDiscs(num - 1, fromPeg, tempPeg, toPeg);
+            System.out.println("Move a disc from peg " + fromPeg + " to peg " + toPeg);
+
+            moveDiscs(num - 1, tempPeg, toPeg, fromPeg);
+        }
+    }
+}
